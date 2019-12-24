8c2ff1727db14f1073511b3ef1af381286747475
Shengjing Zhu <i@zhsj.me>
2019-12-25 00:43:53 +0800

    [sshd] clean up

--- sshd/entrypoint.sh
+++ sshd/entrypoint.sh
@@ -2,22 +2,26 @@
 
 set -e
 
-user="${SSH_USER:-root}"
-password="${SSH_PASSWORD:-$(perl -e 'print map{("a".."z","A".."Z",0..9)[int(rand(62))]}(1..12)')}"
+case "$1" in
+  /usr/sbin/sshd)
+    user="${SSH_USER:-root}"
+    password="${SSH_PASSWORD:-$(perl -e 'print map{("a".."z","A".."Z",0..9)[int(rand(62))]}(1..12)')}"
 
-printf 'Username: %s\nPassword: %s\n' "$user" "$password"
+    printf 'Username: %s\nPassword: %s\n' "$user" "$password"
 
-if [ "$user" = "root" ]; then
-    sed -i '/PermitRootLogin/ c PermitRootLogin yes' /etc/ssh/sshd_config
-fi
+    if [ "$user" = "root" ]; then
+      sed -i '/PermitRootLogin/ c PermitRootLogin yes' /etc/ssh/sshd_config
+    fi
 
-if ! getent passwd "$user" >/dev/null; then
-    adduser --quiet --disabled-password --gecos '' "$user"
-fi
+    if ! getent passwd "$user" >/dev/null; then
+      adduser --quiet --disabled-password --gecos '' "$user"
+    fi
 
-echo "$user:$password" | chpasswd
+    echo "$user:$password" | chpasswd
 
-mkdir -p /run/sshd
-chmod 0755 /run/sshd
+    mkdir -p /run/sshd
+    chmod 0755 /run/sshd
+  ;;
+esac
 
 exec "$@"
