ebfc98ce79599168fb0d6206f52ac2acf84b55ce
Shengjing Zhu <i@zhsj.me>
2021-07-16 22:59:50 +0800

    [goproxy] bump to 0.7.1

--- goproxy/go.mod
+++ goproxy/go.mod
@@ -2,4 +2,4 @@ module main
 
 go 1.14
 
-require github.com/goproxy/goproxy v0.2.2
+require github.com/goproxy/goproxy v0.7.1
--- goproxy/main.go
+++ goproxy/main.go
@@ -4,11 +4,10 @@ import (
 	"net/http"
 
 	"github.com/goproxy/goproxy"
-	"github.com/goproxy/goproxy/cacher"
 )
 
 func main() {
-	s := goproxy.New()
-	s.Cacher = &cacher.Disk{Root: "/cache"}
-	http.ListenAndServe(":8080", s)
+	http.ListenAndServe(":8080", &goproxy.Goproxy{
+		Cacher: goproxy.DirCacher("/cache"),
+	})
 }
