4ce9fbc7c08a411f6325e227984e468b0399b1c0
Shengjing Zhu <i@zhsj.me>
2022-08-26 02:00:49 +0800

    dind: run on kubernetes without privileged

--- dind/README.md
+++ dind/README.md
@@ -1,5 +1,9 @@
 # Dind
 
-Rootless Docker in Docker, with fuse-overlayfs.
+Rootless Docker in Docker.
 
 TLS is disabled.
+
+Need kernel 5.11, to use overlayfs in user namespace.
+
+To run in Kubernetes, see [`gha/example`](../gha/examples).
--- dind/dockerd-entrypoint.sh
+++ dind/dockerd-entrypoint.sh
@@ -10,13 +10,27 @@ if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
 fi
 
 if [ "$1" = 'dockerd' ]; then
+	if [ "$(id -u)" = '0' ]; then
+		mkdir -p /unmasked-proc
+		mount -t proc proc /unmasked-proc
+		mkdir -p /unmasked-sys
+		mount -t sysfs sysfs /unmasked-sys
+		mkdir -p /dev/net
+		mknod /dev/net/tun c 10 200 || :
+		chmod 666 /dev/net/tun
+		exec setpriv --reuid=rootless --regid=rootless \
+			--init-groups --reset-env "$0" "$@"
+	fi
+
 	find /run /var/run -iname 'docker*.pid' -delete || :
 
 	uid="$(id -u)"
 	: "${XDG_RUNTIME_DIR:=/run/user/$uid}"
-	export XDG_RUNTIME_DIR
+	PATH=/usr/local/sbin:/usr/sbin:/sbin:${PATH}
+	export XDG_RUNTIME_DIR PATH
 
 	exec dumb-init rootlesskit \
+		--pidns \
 		--disable-host-loopback \
 		--net=slirp4netns \
 		--port-driver=builtin \
